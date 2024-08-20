d4a67996c8265916ba3a8ce5544e735b9b91fb5c
adubasava <anzhalikad@gmail.com>
2024-08-20 15:47:57 +0300

    Implement form validation

--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,16 @@
 ﻿const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
+
+const alphaNumErr =
+  "must only contain letters and digits and can not be empty!";
+
+const validateCategory = [
+  body("name")
+    .trim()
+    .isAlphanumeric()
+    .withMessage(`Category name ${alphaNumErr}`),
+];
 
 const tripController = require("../controllers/tripController");
 
@@ -7,13 +18,13 @@ router.get("/", tripController.browseCategories);
 
 router.get("/new", tripController.newCategoryForm);
 
-router.post("/new", tripController.creatNewCategory);
+router.post("/new", [validateCategory], tripController.creatNewCategory);
 
 router.get("/:id", tripController.renderCategory);
 
 router.get("/:id/edit", tripController.editCategoryForm);
 
-router.post("/:id/edit", tripController.editCategory);
+router.post("/:id/edit", [validateCategory], tripController.editCategory);
 
 router.get("/:id/delete", tripController.deleteCategory);
 
--- routes/tour.js
+++ routes/tour.js
@@ -1,5 +1,21 @@
 ﻿const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
+
+const alphaNumErr =
+  "must only contain letters and digits and can not be empty!";
+const urlErr = "is not valid URL!";
+
+const validateTour = [
+  body("title").trim().isAlphanumeric().withMessage(`Tour name ${alphaNumErr}`),
+  body("location").optional().trim(),
+  body("description").optional().trim(),
+  body("imageurl")
+    .optional({ values: "falsy" })
+    .trim()
+    .isURL()
+    .withMessage(`Tour URL ${urlErr}`),
+];
 
 const tripController = require("../controllers/tripController");
 
@@ -7,12 +23,13 @@ router.get("/", tripController.getTours);
 
 router.get("/new", tripController.newTourForm);
 
-router.post("/new", tripController.creatNewTour);
+router.post("/new", [validateTour], tripController.creatNewTour);
 
 router.get("/:id", tripController.renderTour);
 
 router.get("/:id/edit", tripController.editTourForm);
-router.post("/:id/edit", tripController.editTour);
+
+router.post("/:id/edit", [validateTour], tripController.editTour);
 
 router.get("/:id/delete", tripController.deleteTour);
 
